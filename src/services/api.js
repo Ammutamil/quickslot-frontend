409760a136a1d810c71622fd745a5bd0d22996ca
Ammu tamil <tamilselvi18997@mail.com>
2026-04-17 16:22:10 +0530

    prod 404 fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 // src/services/api.js
 import axios from "axios";
-
+const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
  //baseURL: "http://localhost:5000/api",
- baseURL: "https://quickslot-backend.onrender.com/api",
+ baseURL: API_URL,
 });
 
 export default api;
